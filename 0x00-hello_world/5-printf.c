ed1ef544f64afb4198f4d368628a72c075795e6d
Martin Mulang <mulangmartin92@gmail.com>
2023-02-16 15:59:20 +0300

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - program to use printf function and pass -Wall gcc test.
+ * return (0) SUCCESS.
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
+
