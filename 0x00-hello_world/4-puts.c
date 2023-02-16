eed1042ae55135938576837bcb839f560db8b76f
Martin Mulang <mulangmartin92@gmail.com>
2023-02-16 15:06:12 +0300

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main A program that prints a line with
+ * puts option.
+ * Return: 0(Success)
+*/
+int main()
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return 0;	
+}
