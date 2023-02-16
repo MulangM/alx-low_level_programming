96d2c85ba11a6176ad4e6583f520db453de94247
Martin Mulang <mulangmartin92@gmail.com>
2023-02-16 15:10:36 +0300

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
  * puts option.
  * Return: 0(Success)
 */
-int main()
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts(""Programming is like building a multilingual puzzle"\n");
 	return 0;	
 }
