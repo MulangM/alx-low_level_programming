9c690eb1a99ab7bc6d3b39a158bf7dc19b2e6190
Martin Mulang <mulangmartin92@gmail.com>
2023-02-17 21:28:47 +0300

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Single Digit Numbers base 10
+ * Return: Always 0 (SUCCESS)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+	printf("%d", i);
+putchar('/');
+return (0);
+}
