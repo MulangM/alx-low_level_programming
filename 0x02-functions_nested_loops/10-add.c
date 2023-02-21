a5dcd811b8681ffa3983fabdb9511675af96d90c
Martin Mulang <mulangmartin92@gmail.com>
2023-02-21 22:11:32 +0300

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add -  a function that prints the sum of two numbers.
+ * @a: first parameter.
+ * @b: second parameter.
+ * Description : sum of two numbers.
+ * Return: a + b.
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
+
