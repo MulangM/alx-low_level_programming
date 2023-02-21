f32c19a3684c35441da4496a914855599b8c9d53
Martin Mulang <mulangmartin92@gmail.com>
2023-02-21 13:43:27 +0300

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "_putchar.c"
+
+/**
+ * main - Entry Point.
+ * Description: Prints _putchar
+ * Return: Always 0. (SUCCESS)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+void print_alphabet(void)
+{	char c;
+	for (char c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
