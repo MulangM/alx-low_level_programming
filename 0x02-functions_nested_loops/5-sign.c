bb56474da4d276354f4dfc5281d2445c372fd203
Martin Mulang <mulangmartin92@gmail.com>
2023-02-21 17:18:02 +0300

    5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,10 +10,13 @@
 int print_sign(int n)
 {
 	if (n < 0)
+		_putchar(-1);
 		return (-1);
 	else if (n == 0)
+		_putchar(0);
 		return (0);
 	else if (n > 0)
+		_putchar(1);
 		return (+1);
 
 	return (0);
