c74e24a72f0b77afcec39d4c081bff143702d803
Martin Mulang <mulangmartin92@gmail.com>
2023-02-21 21:42:10 +0300

    9. Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * times_table - function that prints 9 * 9 multip. table
+ * Return: 4 * 4 table.
+ */
+void times_table(void)
+{
+	int i, j = 0;
+
+	while (j <= 9)
+	{
+		for (i = 0; i < 9; i++)
+		{
+			_putchar(mult + '0');
+			_putchar(', ');
+			_putchar('\n')
+		}
+		j++
+	}
+}
