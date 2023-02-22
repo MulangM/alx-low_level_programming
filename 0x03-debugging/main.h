95b8697a6b35c29cab669f7987c600508c9d7d73
Martin Mulang <mulangmartin92@gmail.com>
2023-02-22 22:45:43 +0300

    0. Multiple mains

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,3 +1,13 @@
-int positive_or_negative(int);
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
+
+//int positive_or_negative(int);
+
 int largest_number(int a, int b, int c);
 
