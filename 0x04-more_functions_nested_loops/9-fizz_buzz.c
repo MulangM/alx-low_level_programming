d8c5887c9206eef251e4adf4d584bfc387443a4d
Martin Mulang <mulangmartin92@gmail.com>
2023-02-23 21:46:38 +0300

    9. Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description : prints buzz and fuzz and some numbers.
+ * Return: 0 always.
+ */
+
+int main(void)
+{
+	int n = 100;
+	int i;
+
+	for  (i = 1; i <= n; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
