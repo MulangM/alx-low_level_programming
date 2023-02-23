0d37d08f642bffd7b07ba30b4778babca52c0c99
Martin Mulang <mulangmartin92@gmail.com>
2023-02-23 22:19:08 +0300

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,25 +9,29 @@
 int main(void)
 {
 	int n = 100;
-	int i;
+	int i, j;
 
-	for  (i = 1; i <= n; i++)
+	for (i = 1; i <= n; i++)
 	{
+		if (j < i)
+		{
+			printf(" ");
+		}
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
 		}
 	}
 	printf("\n");
