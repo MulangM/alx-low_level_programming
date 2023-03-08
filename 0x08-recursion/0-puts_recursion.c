0de040c23e83e182963e3873f920a041a86109fd
Martin Mulang <mulangmartin92@gmail.com>
2023-03-08 20:29:08 +0300

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _puts_recursion- prints a line using recursion.
@@ -8,13 +9,12 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-
-	while (s[i] != '\0')
+	if (*s == '\0')
 	{
-	_putchar(s[i]);
-	i++;
+		_putchar('\n');
+		return;
 	}
-	_putchar('\n');
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
