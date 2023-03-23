ce70896e2fa34d4ea5410767a14bccdab89c325d
Martin Mulang <mulangmartin92@gmail.com>
2023-03-23 22:58:56 +0300

    0. Beauty is variable, ugliness is constant

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers- a function that prints numbers
+ * followed by a new line.
+ * @separator: This is the seperator.
+ * @n: The number of arguments.
+ * Return: Nothing for a void function:
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	int x;
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(args, int);
+		printf("%d", x);
+
+		if (separator != NULL)
+		{
+			if (i < n - 1)
+			{
+				printf("%c ", *separator);
+			}
+		}
+	}
+	printf("\n");
+
+	va_end(args);
+}
